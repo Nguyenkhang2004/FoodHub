8e771bf2e413ab18d75236de70f7095304d471f3
Nguyen Khang <khangdnde180417@fpt.edu.vn>
2025-05-24 18:09:21 +0700

    add environment variable

--- src/main/java/com/example/FoodHub/FoodHubApplication.java
+++ src/main/java/com/example/FoodHub/FoodHubApplication.java
@@ -1,5 +1,6 @@
 package com.example.FoodHub;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodHubApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Optional: Set variables manually to system env
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(FoodHubApplication.class, args);
 	}
 
---
+++ target/generated-sources/annotations/com/example/FoodHub/mapper/UserMapperImpl.java
@@ -0,0 +1,53 @@
+package com.example.FoodHub.mapper;
+
+import com.example.FoodHub.dto.request.UserCreationRequest;
+import com.example.FoodHub.dto.request.UserUpdateRequest;
+import com.example.FoodHub.dto.response.UserResponse;
+import com.example.FoodHub.entity.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-23T18:07:11+0700",
+    comments = "version: 1.6.0.Beta1, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User toUser(UserCreationRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.email( request.getEmail() );
+        user.password( request.getPassword() );
+
+        return user.build();
+    }
+
+    @Override
+    public UserResponse toUserResponse(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserResponse.UserResponseBuilder userResponse = UserResponse.builder();
+
+        userResponse.email( user.getEmail() );
+
+        return userResponse.build();
+    }
+
+    @Override
+    public void updateUser(User user, UserUpdateRequest request) {
+        if ( request == null ) {
+            return;
+        }
+
+        user.setEmail( request.getEmail() );
+    }
+}
