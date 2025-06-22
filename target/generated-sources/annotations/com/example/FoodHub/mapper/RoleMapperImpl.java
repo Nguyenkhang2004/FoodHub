ac1552aa9a32ae1d9a10b08c361da1b5a2b932ad
Nguyen Khang <khangdnde180417@fpt.edu.vn>
2025-06-22 22:51:15 +0700

    update

--- target/generated-sources/annotations/com/example/FoodHub/mapper/RoleMapperImpl.java
+++ target/generated-sources/annotations/com/example/FoodHub/mapper/RoleMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-22T13:19:11+0700",
+    date = "2025-06-22T18:45:02+0700",
     comments = "version: 1.6.0.Beta1, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 @Component
