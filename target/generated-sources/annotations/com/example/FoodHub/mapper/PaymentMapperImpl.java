3f5522a79e40f522cce8166086b31fbded2203ce
thang1684NT <namthang16082004@gmail.com>
2025-06-23 08:00:00 +0700

    23/6

--- target/generated-sources/annotations/com/example/FoodHub/mapper/PaymentMapperImpl.java
+++ target/generated-sources/annotations/com/example/FoodHub/mapper/PaymentMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-22T23:47:51+0700",
-    comments = "version: 1.6.0.Beta1, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
+    date = "2025-06-23T07:53:58+0700",
+    comments = "version: 1.6.0.Beta1, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
 )
 @Component
 public class PaymentMapperImpl implements PaymentMapper {
